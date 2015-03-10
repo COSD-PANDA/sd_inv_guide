1461d3a5a448aa40f81001edccffc2840538dc2f
Maksim Pecherskiy <max@maksimize.com>
2015-03-09 23:22:50 -0700

    Updating package.json

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 var express = require('express')
 var app = express();
+var auth = require('http-auth');
+var basic = auth.basic({
+  realm: "SD Information Coordinators"
+}, function (username, password, callback) { // Custom authentication method.
+  callback(username === "DataSD" && password === "Inventory2015");
+});
 
 app.set('port', (process.env.PORT || 5000))
+app.use(auth.connect(basic));
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
