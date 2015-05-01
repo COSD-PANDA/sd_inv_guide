3899c1e67f12cd1bdd363e0d6a79e2fdc25d1061
Maksim Pecherskiy <max@maksimize.com>
2015-05-01 15:10:46 -0700

    Adding dashboard.

--- invdash/js/main.js
+++ invdash/js/main.js
@@ -39,6 +39,47 @@ $(document).ready(function(){
       wanted: ['i2']
     });
   };
+  var step2 = function() {
+    console.log('s2');
+    var gauges = new Array();
+    $('#dept-search').val('');
+    Tabletop.init({ key: '1FAQK4XC11BFXcczEy-Dfhl1dHCgoDMrubz7jg8gi484',
+      callback: function(data, tabletop) {
+        _.each(data, function(element, index) {
+          // Find box.
+          var el = $('.chart-wrapper:eq(' + index + ')');
+          $('.chart-title', el).html(element['Department']);
+          $('.chart-stage', el).attr('id', 'stageId-' + index);
+          gauges.push(new JustGage({
+            id: 'stageId-' + index,
+            value: element['Step 2 %'],
+            min: 0,
+            max: 100,
+            title: "Step 2 Completion",
+            label: "%",
+            levelColors: ['#bd1f30', '#FFFF00', '#008000']
+          }));
+        });
+        $('#dept-search').keyup(function(event) {
+          console.log(event);
+          console.log($(this).val());
+          var search = ($(this).val()).toLowerCase();
+          $('.chart-title').each(function() {
+            var dText = ($(this).text()).toLowerCase();
+            if(dText.search(search) > -1) {
+              $(this).parent().show();
+            }
+            else {
+              $(this).parent().hide();
+            }
+          });
+        });
+      },
+      simpleSheet: true,
+      wanted: ['i2']
+    });
+  };
+
 
   var step1fun = function() {
 
@@ -47,6 +88,10 @@ $(document).ready(function(){
 
   if($('body').hasClass('step1'))
     step1();
+
+  if($('body').hasClass('step2'))
+    step2();
+
 
 
 
