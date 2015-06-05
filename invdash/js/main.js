8157df9365c384bc88f8474018b6ce4c78809e8a
Maksim Pecherskiy <max@maksimize.com>
2015-06-05 16:13:10 -0700

    Update.

--- invdash/js/main.js
+++ invdash/js/main.js
@@ -1,5 +1,45 @@
 $(document).ready(function(){
 
+  var overall = function() {
+    var gauges = new Array();
+    $('#dept-search').val('');
+    Tabletop.init({ key: '1FAQK4XC11BFXcczEy-Dfhl1dHCgoDMrubz7jg8gi484',
+      callback: function(data, tabletop) {
+        _.each(data, function(element, index) {
+          // Find box.
+          var el = $('.chart-wrapper:eq(' + index + ')');
+          $('.chart-title', el).html(element['Department']);
+          $('.chart-stage', el).attr('id', 'stageId-' + index);
+          gauges.push(new JustGage({
+            id: 'stageId-' + index,
+            value: element['Inventory %'],
+            min: 0,
+            max: 100,
+            title: "Inventory Completion",
+            label: "%",
+            levelColors: ['#bd1f30', '#FFFF00', '#008000']
+          }));
+        });
+        $('#dept-search').keyup(function(event) {
+          console.log(event);
+          console.log($(this).val());
+          var search = ($(this).val()).toLowerCase();
+          $('.chart-title').each(function() {
+            var dText = ($(this).text()).toLowerCase();
+            if(dText.search(search) > -1) {
+              $(this).parent().show();
+            }
+            else {
+              $(this).parent().hide();
+            }
+          });
+        });
+      },
+      simpleSheet: true,
+      wanted: ['i2']
+    });
+  };
+
   var step1 = function() {
     var gauges = new Array();
     $('#dept-search').val('');
@@ -79,6 +119,47 @@ $(document).ready(function(){
       wanted: ['i2']
     });
   };
+  var step3 = function() {
+    console.log('s3');
+    var gauges = new Array();
+    $('#dept-search').val('');
+    Tabletop.init({ key: '1FAQK4XC11BFXcczEy-Dfhl1dHCgoDMrubz7jg8gi484',
+      callback: function(data, tabletop) {
+        _.each(data, function(element, index) {
+          // Find box.
+          var el = $('.chart-wrapper:eq(' + index + ')');
+          $('.chart-title', el).html(element['Department']);
+          $('.chart-stage', el).attr('id', 'stageId-' + index);
+          gauges.push(new JustGage({
+            id: 'stageId-' + index,
+            value: element['Step 3 %'],
+            min: 0,
+            max: 100,
+            title: "Step 3 Completion",
+            label: "%",
+            levelColors: ['#bd1f30', '#FFFF00', '#008000']
+          }));
+        });
+        $('#dept-search').keyup(function(event) {
+          console.log(event);
+          console.log($(this).val());
+          var search = ($(this).val()).toLowerCase();
+          $('.chart-title').each(function() {
+            var dText = ($(this).text()).toLowerCase();
+            if(dText.search(search) > -1) {
+              $(this).parent().show();
+            }
+            else {
+              $(this).parent().hide();
+            }
+          });
+        });
+      },
+      simpleSheet: true,
+      wanted: ['i2']
+    });
+  };
+
 
 
   var step1fun = function() {
@@ -91,6 +172,13 @@ $(document).ready(function(){
 
   if($('body').hasClass('step2'))
     step2();
+
+  if($('body').hasClass('step3'))
+    step3();
+
+  if($('body').hasClass('overall'))
+    overall()
+
 
 
 
